f3d95552b6e55e5dd3352ce450cb5bc365b754d7
pszdev30 <pszjukalkar30@gmail.com>
2020-07-25 12:05:45 -0400

    setup local PostgreSQL database + created Clip table

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ import wave
 import os
 from pydub import AudioSegment
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
 
 import speech_recognition as sr
 
@@ -21,15 +22,49 @@ import speech_recognition as sr
 
 app = Flask(__name__)
 
+env = 'dev'
+
+if(env == 'dev'):
+    app.debug = True
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@localhost/SASearch'
+else:
+    app.debug = False
+    app.config['SQLALCHEMY_DATABASE_URI'] = ''
+
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+class Clip(db.Model):
+    __tablename__ = 'clip'
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(200), unique = True)
+    short_path = db.Column(db.String(200), unique = True)
+    text = db.Column(db.Text(), unique = True)
+
+    def __init__(self, name, short_path, text):
+        self.name = name
+        self.short_path = short_path
+        self.text = text
+
+
+
 @app.route('/')
 def landingPage():
     return render_template('index.html')
 
-if __name__ == '__main__':
-    app.run(debug=True)
+@app.route('/search', methods =['GET'])
+def search(key):
+    if(request.method == 'GET'):
+        print('HELLO')
 
-r = sr.Recognizer()
 
-with sr.WavFile("we_do_not_care.wav") as source:
-    audio = r.record(source)
-    print(r.recognize_google(audio))
+
+if(__name__ == '__main__'):
+    app.run()
+
+# r = sr.Recognizer()
+#
+# with sr.WavFile("we_do_not_care.wav") as source:
+#     audio = r.record(source)
+#     print(r.recognize_google(audio))
