544ffdc8c7ed19d25c7b2fb4b18ff1140116933a
pszdev30 <pszjukalkar30@gmail.com>
2020-08-18 12:11:38 -0400

    cleaned up app component

--- SASearch-web/src/app/app.component.ts
+++ SASearch-web/src/app/app.component.ts
@@ -1,6 +1,6 @@
-import { FlaskService } from './service/flask.service';
+
 import { Component, OnInit } from '@angular/core';
-import { Cloudinary } from '@cloudinary/angular-5.x';
+
 
 @Component({
   selector: 'app-root',
@@ -9,47 +9,7 @@ import { Cloudinary } from '@cloudinary/angular-5.x';
 })
 export class AppComponent implements OnInit {
   title = 'SASearch-web';
-  query: string;
-  publicId: string;
-  publicIds: any[] = [];
-  searchClicked: boolean = false;
-  randomClicked: boolean = false;
-
-  constructor(private flaskService: FlaskService, private cloudinary: Cloudinary) { }
 
   ngOnInit() { }
-
-  setQuery(query: string) {
-    this.query = query;
-  }
-
-  search(query: string) {
-    this.flaskService.search(query).subscribe((resp: any) => {
-      let respLength: number = Object.keys(resp).length
-      for (let i = 0; i < respLength; i++) {
-        this.publicIds.push(resp[i]);
-        console.log(this.publicIds[i])
-      }
-      this.searchClicked = true;
-    });
-    this.reset();
-  }
-
-  getRandom() {
-    this.flaskService.getRandom().subscribe((resp: string) => {
-      console.log(resp);
-      this.publicId = resp;
-      this.randomClicked = true;
-    });
-    this.reset();
-  }
-
-
-  reset() {
-    this.publicId = '';
-    this.publicIds = [];
-    this.searchClicked = false;
-    this.randomClicked = false;
-  }
 
 }
